cd8e7093fe093f15932abf13556a64adb53a9b96
Oleg <frontendercode@gmail.com>
2019-07-26 09:53:26 +0300

    v.0.1
    Собрал стартовый пакет для создания темы mycompany. Создал основу для нарезки шаблона

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# MycompanyWordpressAcfpro
-Выполняю создание темы WordPress с использованием плагина Acf Pro на основе шаблона mycompany
+# Создание собственнной темы mycompany
+
+Выполняю создание темы WordPress с использованием плагина Acf Pro на основе шаблона mycompany. В работе испрользую базовый стартовый шаблон, его сгенерировал с помощью сервиса underscores.me, c использованием плагина ACF PRO WordPress.
+
+В каталоге myco находятся файлы темы
+Исходные файлы шаблона находятся в другом репозитории моего Githab. Для ознакомления с ним перейдите по этой [ссылке] https://github.com/frontend-coder/mycompany
+
+В процессе работы создается набор сниппетов для создания темы.
+
+Шаблон mycompany представляет собой простой Landing Page. Внутренние шаблоны темы не разработаны создателем макета.
+
+
+## Связаться по вопросам создания сайта под ключ:
+
+- [facebook](https://www.facebook.com/frontendercode)
+- [github](https://github.com/frontend-coder)
+- [skype:webrabcom](href="skype:webrabcom")
+- [telegram](https://t.me/frontendcoder)
+
+## Портфолио
+
+- [Портфолио](https://frontend-coder.github.io)
---
+++ myco/inc/function-noupdate.php
@@ -0,0 +1,42 @@
+<?php
+
+/**Отключаем обновления плагинов, тем в админке
+* Отключаем принудительную проверку новых версий WP, плагинов и темы в админке,
+* чтобы она не тормозила, когда долго не заходил и зашел...
+* Все проверки будут происходить незаметно через крон или при заходе на страницу: "Консоль > Обновления".
+*
+* @see https://wp-kama.ru/filecode/wp-includes/update.php
+* @author Kama (https://wp-kama.ru)
+* @version 1.0
+*/
+if( is_admin() ){
+// отключим проверку обновлений при любом заходе в админку...
+remove_action( 'admin_init', '_maybe_update_core' );    // (ats) откл. проверки обнов. ядра WP
+remove_action( 'admin_init', '_maybe_update_plugins' ); // (ats) откл. проверки обнов. плагинов
+remove_action( 'admin_init', '_maybe_update_themes' );  // (ats) откл. обнов тем (шаблонов)
+
+// отключим проверку обновлений при заходе на специальную страницу в админке...
+remove_action( 'load-plugins.php', 'wp_update_plugins' );
+remove_action( 'load-themes.php', 'wp_update_themes' );
+
+// оставим принудительную проверку при заходе на страницу обновлений...
+//remove_action( 'load-update-core.php', 'wp_update_plugins' );
+//remove_action( 'load-update-core.php', 'wp_update_themes' );
+
+// внутренняя страница админки "Update/Install Plugin" или "Update/Install Theme" - оставим не мешает...
+//remove_action( 'load-update.php', 'wp_update_plugins' );
+//remove_action( 'load-update.php', 'wp_update_themes' );
+
+// событие крона не трогаем, через него будет проверяться наличие обновлений - тут все отлично!
+//remove_action( 'wp_version_check', 'wp_version_check' );
+//remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+//remove_action( 'wp_update_themes', 'wp_update_themes' );
+
+/**
+* отключим проверку необходимости обновить браузер в консоли - мы всегда юзаем топовые браузеры!
+* эта проверка происходит раз в неделю...
+* @see https://wp-kama.ru/function/wp_check_browser_version
+*/
+add_filter( 'pre_site_transient_browser_'. md5( $_SERVER['HTTP_USER_AGENT'] ), '__return_true' );
+}
+/** Отключаем обновления плагинов, тем в админке **/
